22e5955c2429791215c7ecb9f61989a578675f30
Alec Savvy <alecjsavvy@gmail.com>
2023-03-06 19:13:59 -0700

    add serde derives to entity manager

--- src/entity_manager.rs
+++ src/entity_manager.rs
@@ -2,5 +2,6 @@ ethcontract::contract! {
     pub "EntityManager.json",
     methods {
         initialize(address, uint256) as other_initialize
-    }
+    },
+    event_derives (serde::Deserialize, serde::Serialize),
 }
