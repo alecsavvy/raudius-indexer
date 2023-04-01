489486e16323a3965efb8392da47101660538f8d
Alec Savvy <alecjsavvy@gmail.com>
2023-04-01 09:02:48 -0600

    add faker

--- gen.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-    -i https://discoveryprovider.audius.co/v1/swagger.json \
-    -g rust \
-    -o /local/src/api \
-    --skip-validate-spec \
-    --global-property models,supportingFiles
